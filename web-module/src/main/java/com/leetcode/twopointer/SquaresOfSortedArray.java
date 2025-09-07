d138edf688c27b81c9d68f038380320b66b56d1b
vndeepthi <deepthi.naga@gmail.com>
2025-09-07 17:17:53 -0500

    submit

--- web-module/src/main/java/com/leetcode/twopointer/SquaresOfSortedArray.java
+++ web-module/src/main/java/com/leetcode/twopointer/SquaresOfSortedArray.java
@@ -1,9 +1,47 @@
 package com.leetcode.twopointer;
 
+import java.util.Arrays;
+
 /**
  * Given an integer array nums sorted in non-decreasing order,
  * return an array of the squares of each number sorted in non-decreasing order.
  */
 public class SquaresOfSortedArray {
 
+    public static int[] sortedSquares(int[] nums) {
+        int left = 0;
+        while (nums[left] < 0 ) {
+            left++;
+        }
+        int right = left + 1;
+        int[] result = new int[nums.length];
+        int resultIndex = 0;
+        while (left >=0 && right < nums.length) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare > rightSquare) {
+                result[resultIndex++] = rightSquare;
+                right++;
+            } else {
+                result[resultIndex++] = leftSquare;
+                left--;
+            }
+        }
+        while (left >=0 ) {
+            result[resultIndex++] = nums[left] * nums[left];
+            left--;
+        }
+        while (right < nums.length) {
+            result[resultIndex++] = nums[right] * nums[right];
+            right++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-4,-1,0,3,10})));
+        //System.out.println(Arrays.toString(sortedSquares(new int[]{-7,-3,2,3,11})));
+        //System.out.println(Arrays.toString(sortedSquares(new int[]{-1,0, 1})));
+    }
 }
+
