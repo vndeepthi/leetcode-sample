d7264886ac9149259031b53ad95c57b7fd94037b
vndeepthi <deepthi.naga@gmail.com>
2025-09-07 12:10:20 -0500

    test commit from new mac.

---
+++ web-module/src/main/java/com/leetcode/twopointer/MoveZeros.java
@@ -0,0 +1,13 @@
+package com.leetcode.twopointer;
+
+/**
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ *
+ * Note that you must do this in-place without making a copy of the array.
+ */
+public class MoveZeros {
+
+    public void moveZeroes(int[] nums) {
+
+    }
+}
